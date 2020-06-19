59ee201315007d94220934952c41da0107a948ff
ziolokeven <ziolokeven@gmail.com>
2020-06-19 12:42:00 +0200

    wyslanie

--- src/app/components/orders-rz/orders-rz.component.ts
+++ src/app/components/orders-rz/orders-rz.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {RZDataService} from "../../services/rz-data.service";
 
 @Component({
-  selector: 'app-orders-rz',
+  selector: 'orders-rz',
   templateUrl: './orders-rz.component.html',
   styleUrls: ['./orders-rz.component.css']
 })
 export class OrdersRZComponent implements OnInit {
-
-  constructor() { }
+  public item$: any;
+  items$: any;
+  constructor(private service : RZDataService) { }
 
   ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll(){
+    this.service.getALL().subscribe(response =>{
+      this.item$ = response;
+    })
   }
 
 }
